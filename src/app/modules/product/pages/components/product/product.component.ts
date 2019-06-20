1164b1cf462906318d11eb732200636a035f647a
Nich-at-790999098857 <nich.t.j@gmail.com>
2019-06-20 11:06:12 -0400

    updates price tag format to better align with the demo products page.

--- src/app/modules/product/pages/components/product/product.component.ts
+++ src/app/modules/product/pages/components/product/product.component.ts
@@ -10,10 +10,19 @@ import { addToCart } from '../../../models/product.model';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+    public PriceTags: PriceTag[] = [];
     @Input('product')
 
     public set product(p: Product) {
-        console.log("Setting a new product ", p);
+        //console.log("Setting a new product ", p);
+        this.PriceTags = [];
+        if (p.price2) {
+            this.PriceTags.push({ price: p.price2, cuom: (p.cuom2) });
+        }
+        if (p.price1) {
+            this.PriceTags.push({ price: p.price1, cuom: (p.cuom1) });
+        }
+        //console.log('Prices', this.PriceTags);
         this._product = p;
     }
     public get product(): Product {
@@ -30,3 +39,7 @@ export class ProductComponent implements OnInit {
 
     }
 }
+export type PriceTag = {
+    price: number,
+    cuom?: string
+};
